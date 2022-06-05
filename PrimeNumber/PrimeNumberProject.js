4e1f576d33ed27acb8eb8f42e04945ec27bb8083
neshat <101034564+neeeshat@users.noreply.github.com>
2022-06-05 15:17:05 +0430

    upload Prime Number

---
+++ PrimeNumber/PrimeNumberProject.js
@@ -0,0 +1,28 @@
+//finding the prime number
+// with decreasing the run time by using break 
+// and using i < parseInt(num**0.5)+1 instead of i<n
+// test with this 1000000007  which is a prime number
+
+let num = 1000000007;
+
+let i=2;
+let isPrime =true;
+
+
+while (i < parseInt(num**0.5)+1) 
+{
+	if(num % i ==0)
+	{
+		isPrime =false;
+		console.log(isPrime);
+		console.log(`${num} is divided by ${i}`);
+		break;
+
+	}
+	i ++;	
+}
+if (isPrime)
+	console.log(`${num} is a prime number`);
+else 
+	console.log(`as a result ${num} is not a prime number`);
+		
